cfdef60ad830e270f9696f85bcb6471abd532775
paraglande03 <paraglande03@gmail.com>
2020-12-14 19:28:14 +0530

    Add last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 shopt -s extglob
-read -p "Enter name:" name
+flag=0
+	while [[ flag -eq 0 ]]
+do
+read -p "Enter First name:" name
 
-	pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
-
+pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
 	if [[ $name =~ $pattern ]]
+ 	then
+		flag=1
+	else
+		echo "Invalid Enter Again"
+	fi
+done
+	while [[ flag -eq 1 ]]
+do
+read -p "Enter Last name:" name
 
-then
+pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+        if [[ $name =~ $pattern ]]
+        then
+		flag=2
+        else
+                echo "Invalid Enter Again"
+        fi
+done
 
 
-        echo "valid"
-
-else
-
-        echo "invalid"
-
-fi
 
 
 
