0ab12e63ff2f5d931defc9bff98d24d3b57fde48
paraglande03 <paraglande03@gmail.com>
2020-12-14 19:06:29 +0530

    Add Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
-read -p "Enter Name" name 
+shopt -s extglob
+read -p "Enter name:" name
+
+	pattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+	if [[ $name =~ $pattern ]]
+
+then
+
+
+        echo "valid"
+
+else
+
+        echo "invalid"
+
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
